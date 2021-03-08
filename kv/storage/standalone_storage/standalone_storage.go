9948e7c56e0193a27753d908a57630783ce3f664
IcePigZDB <IcePigZDB@gmail.com>
2021-03-08 16:47:37 +0800

    finish project1

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -1,8 +1,12 @@
 package standalone_storage
 
 import (
+	"path"
+
+	"github.com/Connor1996/badger"
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
@@ -10,11 +14,20 @@ import (
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
 	// Your Data Here (1).
+	engine engine_util.Engines
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
-	return nil
+	kvPath := path.Join(conf.DBPath, "kv")
+	raftPath := path.Join(conf.DBPath, "raft")
+
+	kvEngine := engine_util.CreateDB(kvPath, false)
+	raftEngine := engine_util.CreateDB(raftPath, true)
+
+	engine := engine_util.NewEngines(kvEngine, raftEngine, kvPath, raftPath)
+
+	return &StandAloneStorage{engine: *engine}
 }
 
 func (s *StandAloneStorage) Start() error {
@@ -29,10 +42,48 @@ func (s *StandAloneStorage) Stop() error {
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	// Your Code Here (1).
-	return nil, nil
+	return NewStandAloneReader(s.engine.Kv.NewTransaction(false))
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
 	// Your Code Here (1).
+	for _, b := range batch {
+		switch b.Data.(type) {
+		case storage.Put:
+			err := engine_util.PutCF(s.engine.Kv, b.Cf(), b.Key(), b.Value())
+			if err != nil {
+				return err
+			}
+		case storage.Delete:
+			err := engine_util.DeleteCF(s.engine.Kv, b.Cf(), b.Key())
+			if err != nil {
+				return err
+			}
+		}
+	}
 	return nil
+}
+
+type StandAloneReader struct {
+	kvTxn *badger.Txn
+}
+
+func NewStandAloneReader(kvTxn *badger.Txn) (*StandAloneReader, error) {
+	return &StandAloneReader{kvTxn: kvTxn}, nil
+}
+
+func (r *StandAloneReader) GetCF(cf string, key []byte) (val []byte, err error) {
+	val, err = engine_util.GetCFFromTxn(r.kvTxn, cf, key)
+	if err == badger.ErrKeyNotFound {
+		return nil, nil
+	}
+	return
+}
+
+func (r *StandAloneReader) IterCF(cf string) engine_util.DBIterator {
+	return engine_util.NewCFIterator(cf, r.kvTxn)
+}
+
+func (r *StandAloneReader) Close() {
+	r.kvTxn.Discard()
 }
